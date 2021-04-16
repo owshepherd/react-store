e01f352adece13e6e779648edcd876f5bb747e44
Owen Shepherd <owshepherd@gmail.com>
2021-04-16 15:56:24 +1000

    added shop page, added  collection, collection-item components

--- src/components/directory/directory-component.jsx
+++ src/components/directory/directory-component.jsx
@@ -47,8 +47,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.section.map(({ id, title, imageUrl, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.section.map(({ id, ...sectionProps }) => (
+          <MenuItem key={id} {...sectionProps} />
         ))}
       </div>
     );
--- src/components/menu-item/menu-item-component.jsx
+++ src/components/menu-item/menu-item-component.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.scss';
 
-const MenuItem = ({ title, size, imageUrl }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, size, imageUrl, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       className="background-image"
       style={{ backgroundImage: `url(${imageUrl})` }}
@@ -14,4 +18,4 @@ const MenuItem = ({ title, size, imageUrl }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
