5ceeea7e4c3251fdb28b0ccd15ba20d13c9de22e
Owen Shepherd <owshepherd@gmail.com>
2021-04-17 17:23:27 +1000

    added form and sign-in components, tweaks to scss

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import Header from './components/header/header-component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up-component';
 import HomePage from './pages/homepage/homepage-component';
 import ShopPage from './pages/shop/shop-component';
 
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
